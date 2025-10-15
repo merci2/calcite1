1902fbc33f60792d664b4a2c1a2750a8961b7d3f
merci2 <karganja@yahoo.de>
2025-10-15 09:16:09 +0200

    Update ProjectsPanel.tsx

--- app2/src/components/ProjectsPanel/ProjectsPanel.tsx
+++ app2/src/components/ProjectsPanel/ProjectsPanel.tsx
@@ -1,31 +1,13 @@
-import { useState } from 'react';
-import { CalcitePanel, CalciteInput } from '@esri/calcite-components-react';
-import translations from '../../translations/en/global.json';
-import styles from './ProjectsPanel.module.css';
+//import { CalcitePanel, CalciteInput } from '@esri/calcite-components-react';
+//import translations from '../../translations/en/global.json';
+//import styles from './ProjectsPanel.module.css';
 
 function ProjectsPanel() {
-  const [searchValue, setSearchValue] = useState('');
-
-  const handleSearch = (e: Event) => {
-    const input = e.target as HTMLCalciteInputElement;
-    setSearchValue(input.value);
-    console.log('Suche:', input.value);
-  };
+  
 
   return (
-    <CalcitePanel heading={translations.heading}>
-      <div className={styles.content}>
-        {/* Suchleiste */}
-        <CalciteInput
-          placeholder="Search ..."
-          icon="search"
-          clearable
-          value={searchValue}
-          onCalciteInputInput={handleSearch}
-          className={styles.searchInput}
-        />
-      </div>
-    </CalcitePanel>
+    <>
+    </>
   );
 }
 
